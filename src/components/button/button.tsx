3b52d6975ce12373d2c9ddf26c34750d95166411
Shekhar S <shekharshashank97@gmail.com>
2025-02-11 21:41:03 +0530

    Feat:
    Implemented button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,27 @@
+import { ButtonElement, PrimitveButtonProps } from "@Configs/primitive";
+import { Component, JSX, splitProps } from "solid-js";
+import { Dynamic } from "solid-js/web";
+import { cn } from "@/utils";
+
+// Button props
+interface ButtonWrapperProps extends PrimitveButtonProps {
+    asChild?: keyof JSX.IntrinsicElements | Component<any>;
+    children?: JSX.Element;
+    ref?: (el: ButtonElement) => void;
+    class?: string;
+}
+
+// Button Component
+const Button: Component<ButtonWrapperProps> = (props) => {
+    // separate our special props from the rest
+    const [local, others] = splitProps(props, ["asChild", "ref", "children", "class"]);
+
+    return (
+        <Dynamic component={local.asChild || 'button'} ref={local.ref} class={cn("flex items-center justify-center gap-3  outline-none cursor-pointer p-4 rounded-sm text-[1rem] w-full mx-auto", local?.class)} {...others}>
+            {local.children}
+        </Dynamic>
+    );
+};
+
+// export
+export { Button };
