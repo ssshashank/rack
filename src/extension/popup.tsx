c8e77568d4711e134f0f9212ca0702d1d3c639f4
Shekhar S <shekharshashank97@gmail.com>
2025-02-11 21:42:17 +0530

    Feat:
    Updated popup ui for extension

--- src/extension/popup.tsx
+++ src/extension/popup.tsx
@@ -1,11 +1,35 @@
-import App from "@/App";
+import { Button } from "@Components/button";
+import { createSignal } from "solid-js";
+
+declare const chrome: any;
 
 const Popup = () => {
+    const [isSidePanel, setSidePanel] = createSignal<boolean>(false);
+
+    const togglePanel = async () => {
+        const [tab] = await chrome?.tabs?.query({ active: true, currentWindow: true });
+        if (tab && tab?.id !== undefined) {
+            if (isSidePanel()) {
+                // close the side panel
+                await chrome?.sidePanel?.close({ tabId: tab?.id });
+                setSidePanel(false);
+            } else {
+                // open the side panel
+                await chrome?.sidePanel?.open({ tabId: tab?.id });
+                setSidePanel(true);
+                // close the popup window
+                window.close();
+            }
+        } 
+        console.log(tab);
+    };
+
     return (
-        <div class="">
+        <div class="w-full h-full bg-yellow-100">
             <h1 class="text-blue-600">Extension</h1>
-            <App />
-        </div>);
+            <Button onclick={togglePanel}>Click Here</Button>
+        </div>
+    );
 };
 
-export default Popup;
+export default Popup; 
